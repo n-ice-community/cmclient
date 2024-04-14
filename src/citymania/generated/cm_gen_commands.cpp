fb753aa5ebfb7aaf1759c953321bbda1caa4b1f0
chucky-n-ice <chucky_n-ice@outlook.com>
2024-04-14 15:32:22 +0200

    Change: ottd_survey_uri to default; vstudio hangs with ../../

--- src/citymania/generated/cm_gen_commands.cpp
+++ src/citymania/generated/cm_gen_commands.cpp
@@ -2,14 +2,14 @@
 
 #include "../../stdafx.h"
 #include "cm_gen_commands.hpp"
-#include "../../src/misc_cmd.h"
-#include "../../src/object_cmd.h"
-#include "../../src/order_cmd.h"
-#include "../../src/rail_cmd.h"
-#include "../../src/road_cmd.h"
-#include "../../src/station_cmd.h"
-#include "../../src/town_cmd.h"
-#include "../../src/tunnelbridge_cmd.h"
+#include "../src/misc_cmd.h"
+#include "../src/object_cmd.h"
+#include "../src/order_cmd.h"
+#include "../src/rail_cmd.h"
+#include "../src/road_cmd.h"
+#include "../src/station_cmd.h"
+#include "../src/town_cmd.h"
+#include "../src/tunnelbridge_cmd.h"
 namespace citymania {
 namespace cmd {
 
